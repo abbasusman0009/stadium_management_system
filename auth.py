eda4870289c6a4d97636643fed2638437b2851a4
Abbas <abbasusman0009@gmail.com>
2026-06-07 17:13:48 +0100

    Fix bcrypt import error crashing gunicorn

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 from models import db, User
-from __main__ import bcrypt
+from extensions import bcrypt
 
 auth_bp = Blueprint('auth', __name__)
 
